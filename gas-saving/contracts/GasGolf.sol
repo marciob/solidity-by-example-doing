10def5eb4d43bc2756b7a1a29fc3a9d48b02ec7d
m <13090422+marciob@users.noreply.github.com>
2022-10-30 23:23:07 -0300

    add gas lesson

---
+++ gas-saving/contracts/GasGolf.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// gas golf
+contract GasGolf {
+    uint public total;
+
+    // not gas optimized
+    // it keeps reading 'total' variable from storage, not from the memory
+    // function sumIfEvenAndLessThan99(uint[] memory nums) external {
+    //     for (uint i = 0; i < nums.length; i += 1) {
+    //         bool isEven = nums[i] % 2 == 0;
+    //         bool isLessThan99 = nums[i] < 99;
+    //         if (isEven && isLessThan99) {
+    //             total += nums[i];
+    //         }
+    //     }
+    // }
+
+    // gas optimized
+    // it reads _total from memory, not from storage
+    // [1, 2, 3, 4, 5, 100]
+    function sumIfEvenAndLessThan99(uint[] calldata nums) external {
+        uint _total = total;
+        uint len = nums.length;
+
+        for (uint i = 0; i < len; ) {
+            uint num = nums[i];
+            if (num % 2 == 0 && num < 99) {
+                _total += num;
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        total = _total;
+    }
+}
